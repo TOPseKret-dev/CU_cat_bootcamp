ff6dba53ece6924e7ed6ba2fb6cc3e0b10ff7e63
TOPseKret-dev <barabanovai2007@gmail.com>
2025-02-07 16:09:35 +0300

    add DB and admins functions

---
+++ create_DB.py
@@ -0,0 +1,18 @@
+import aiosqlite
+import asyncio
+
+
+async def create_db():
+    async with aiosqlite.connect("database.db") as db:
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS admins (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER UNIQUE NOT NULL,
+            username TEXT
+        );
+        """)
+        await db.commit()
+    print("База данных успешно создана!")
+
+if __name__ == "__main__":
+    asyncio.run(create_db())
--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import asyncio
+from db_controller import *
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
-from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import State, StatesGroup
-from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup, BufferedInputFile
-from datetime import datetime
-from base64 import b64decode
+from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup
 import time
 import requests
 
@@ -13,154 +10,113 @@ import requests
 from keys import bot_token, folder_id, api_key
 
 
-class YandexGPT:
-    def getAnswer(user_prompt):
-        system_prompt = 'Ты ассистент писателя. Напиши короткий рассказ. В ответе пришли только рассказ.'
-        gpt_model = 'yandexgpt-lite'
-        body = {
-            'modelUri': f'gpt://{folder_id}/{gpt_model}',
-            'completionOptions': {'stream': False, 'temperature': 0.3, 'maxTokens': 2000},
-            'messages': [
-                {'role': 'system', 'text': system_prompt},
-                {'role': 'user', 'text': user_prompt},
-            ]
-        }
-        url = 'https://llm.api.cloud.yandex.net/foundationModels/v1/completionAsync'
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': f'Api-Key {api_key}'
-        }
-        response = requests.post(url, headers=headers, json=body)
-        operation_id = response.json().get('id')
-        url = f"https://llm.api.cloud.yandex.net:443/operations/{operation_id}"
-        headers = {"Authorization": f"Api-Key {api_key}"}
-
-        while True:
-            response = requests.get(url, headers=headers)
-            done = response.json()["done"]
-            if done:
-                break
-            else:
-                time.sleep(1)
-
-        data = response.json()
-        answer = data['response']['alternatives'][0]['message']['text']
-        return answer
-
-
-class YandexArt:
-
-    def getImage(prompt, style):
-        match style:
-            case 'Живопись':
-                system_prompt = 'стиль: живопись'
-            case 'Аниме':
-                system_prompt = 'Стиль Studio Ghibli, Динамичный Shonen'
-
-        seed = int(round(datetime.now().timestamp()))
-        body = {
-            "modelUri": f"art://{folder_id}/yandex-art/latest",
-            "generationOptions": {"seed": seed, "temperature": 0.6},
-            "messages": [
-                {"weight": 1, "text": prompt},
-                {"weight": 1, "text": system_prompt}
-            ],
-        }
-
-        url = "https://llm.api.cloud.yandex.net/foundationModels/v1/imageGenerationAsync"
-        headers = {"Authorization": f"Api-Key {api_key}"}
-
-        response = requests.post(url, headers=headers, json=body)
-        operation_id = response.json()["id"]
-
-        url = f"https://llm.api.cloud.yandex.net:443/operations/{operation_id}"
-        headers = {"Authorization": f"Api-Key {api_key}"}
-
-        while True:
-            response = requests.get(url, headers=headers)
-            done = response.json()["done"]
-            if done:
-                break
-            else:
-                time.sleep(2)
-
-        image_data = response.json()["response"]["image"]
-        image = b64decode(image_data)
-        return image
-
-
-class UserInfo(StatesGroup):
-    mode = State()
-    style = State()
-    prompt = State()
-
-
-async def command_start(message: Message, state: FSMContext) -> None:
-    await state.set_state(UserInfo.mode)
-    kb_list = [
-        [KeyboardButton(text="Придумать рассказ"), KeyboardButton(text="Придумать картинку")],
-    ]
-    await message.answer("Привет, давай придумаем!", reply_markup=ReplyKeyboardMarkup(keyboard=kb_list,
-                                                                                      resize_keyboard=True,
-                                                                                      one_time_keyboard=True))
-
-
-async def process_mode(message: Message, state: FSMContext) -> None:
-    if message.text == "Придумать рассказ":
-        data = await state.update_data(mode=1, style="")
-        await state.set_state(UserInfo.prompt)
-        await message.answer("Хорошо, какая тема будет?")
-    elif message.text == "Придумать картинку":
-        data = await state.update_data(mode=2)
-        await state.set_state(UserInfo.style)
-        kb_list = [
-            [KeyboardButton(text="Живопись"), KeyboardButton(text="Аниме")],
+def get_answer(user_prompt):
+    system_prompt = ''
+    gpt_model = 'yandexgpt-lite'
+    body = {
+        'modelUri': f'gpt://{folder_id}/{gpt_model}',
+        'completionOptions': {'stream': False, 'temperature': 0.3, 'maxTokens': 2000},
+        'messages': [
+            {'role': 'system', 'text': system_prompt},
+            {'role': 'user', 'text': user_prompt},
         ]
-        await message.answer("Хорошо, какой стиль будет?", reply_markup=ReplyKeyboardMarkup(keyboard=kb_list,
-                                                                                            resize_keyboard=True,
-                                                                                            one_time_keyboard=True))
+    }
+    url = 'https://llm.api.cloud.yandex.net/foundationModels/v1/completionAsync'
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Api-Key {api_key}'
+
+    }
+    response = requests.post(url, headers=headers, json=body)
+    operation_id = response.json().get('id')
+    url = f"https://llm.api.cloud.yandex.net:443/operations/{operation_id}"
+    headers = {"Authorization": f"Api-Key {api_key}"}
+
+    while True:
+        response = requests.get(url, headers=headers)
+        done = response.json()["done"]
+        if done:
+            break
+        else:
+            time.sleep(1)
+
+    data = response.json()
+    answer = data['response']['alternatives'][0]['message']['text']
+    return answer
 
 
-async def process_style(message: Message, state: FSMContext) -> None:
-    data = await state.update_data(style=message.text)
-    await state.set_state(UserInfo.prompt)
-    await message.answer("Хорошо, какая тема будет?")
+async def command_start(message: Message) -> None:
+    user_id = message.from_user.id
+    if await is_admin(user_id):
+        kb_list = [[KeyboardButton(text="Настроить ассистента")]]
+        await message.answer("Привет, я твой бот-ассистент, помогу твоим студентам в обучении!",
+                             reply_markup=ReplyKeyboardMarkup(keyboard=kb_list,
+                                                              resize_keyboard=True,
+                                                              one_time_keyboard=False))
+    else:
+        await message.answer("Привет, я бот-ассистент, если у тебя есть вопросы, задай их мне, и я помогу!")
 
 
-async def process_prompt(message: Message, state: FSMContext) -> None:
-    data = await state.update_data(prompt=message.text)
-    kb_list = [[KeyboardButton(text="/start")]]
-    match data["mode"]:
-        case 1:
-            answer = YandexGPT.getAnswer(data["prompt"])
-            await message.answer(answer, reply_markup=ReplyKeyboardMarkup(
-                keyboard=kb_list,
-                resize_keyboard=True,
-                one_time_keyboard=True)
-                                 )
-        case 2:
-            img = YandexArt.getImage(data["prompt"], data["style"])
-            image_file = BufferedInputFile(
-                file=img,
-                filename="generated_image.png"
-            )
-            await message.answer_photo(
-                photo=image_file,
-                reply_markup=ReplyKeyboardMarkup(
-                    keyboard=kb_list,
-                    resize_keyboard=True,
-                    one_time_keyboard=True)
-            )
+async def add_admin_command(message: Message):
+    user_id = message.from_user.id
+    if not is_admin(user_id):
+        await message.reply("Только администраторы могут добавить другого администратора")
+        return
 
-    await state.clear()
+    parts = message.text.split()
+    if len(parts) < 2:
+        await message.reply("Используй: /addadmin <user_id> [username]")
+        return
+
+    try:
+        new_admin_id = int(parts[1])
+    except ValueError:
+        await message.reply("User ID должен быть числом.")
+        return
+
+    username = parts[2] if len(parts) >= 3 else None
+
+    await add_admin(new_admin_id, username)
+    await message.reply("Новый администратор добавлен!")
+
+
+async def remove_admin_command(message: Message):
+    user_id = message.from_user.id
+    if not is_admin(user_id):
+        await message.reply("Только администраторы могут удалять других администраторов")
+        return
+
+    parts = message.text.split()
+    if len(parts) < 2:
+        await message.reply("Используй: /removeadmin <user_id>")
+        return
+
+    try:
+        admin_id = int(parts[1])
+    except ValueError:
+        await message.reply("User ID должен быть числом.")
+        return
+
+    await remove_admin(admin_id)
+    await message.reply("Администратор удалён!")
+
+
+async def get_all_admins(message: Message):
+    user_id = message.from_user.id
+    if not await is_admin(user_id):
+        await message.reply("Только администраторы могут видеть список администраторов")
+        return
+
+    admins_str = await get_admins()
+    await message.reply(f"Список администраторов:\n{admins_str}")
 
 
 async def main() -> None:
     dp = Dispatcher()
     dp.message.register(command_start, Command("start"))
-    dp.message.register(process_mode, UserInfo.mode)
-    dp.message.register(process_style, UserInfo.style)
-    dp.message.register(process_prompt, UserInfo.prompt)
+    dp.message.register(add_admin_command, Command("addadmin"))
+    dp.message.register(remove_admin, Command("removeadmin"))
+    dp.message.register(get_all_admins, Command("getalladmins"))
 
     bot = Bot(token=bot_token)
     await dp.start_polling(bot)
